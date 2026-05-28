44ba1aa075812959d10eb713f76ba4925b6f5041
KingNeoIV <michael.rios.nw@hotmail.com>
2026-05-27 20:16:12 -0500

    refactor(layout): add navbar wrapper and update layout structure

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,16 @@
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen flex flex-col">
+
+            {/* Top Navigation */}
+            <Navbar />
+
+            {/* Page Content */}
             <main className="flex-1">
                 {children}
             </main>
-
-            <Footer />
         </div>
     );
 }
