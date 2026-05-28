ca43260a6b5464ecb3a3f10db4818345aa34f213
KingNeoIV <michael.rios.nw@hotmail.com>
2026-05-27 20:16:46 -0500

    feat(dashboard): create dashboard page with layout integration

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+    return (
+        <div className='
+            bg-slate-900 flex flex-row h-screen
+        '>
+            <div className="bg-red-800">test</div>
+            <div className="bg-green-800">test</div>
+
+        </div>
+    );
+}
