72c347b6237b09500ebced576afcc60b11149a92
KingNeoIV <michael.rios.nw@hotmail.com>
2026-05-27 20:16:23 -0500

    feat(router): wrap dashboard in authenticated layout

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/login';
 import NewUser from '../pages/newUser';
-import ResetPassword from '../pages/ResetPassword'; 
+import ResetPassword from '../pages/ResetPassword';
+import Dashboard from '../pages/dashboard';
+import Layout from '../layout/Layout'; 
 
 export default function AppRouter() {
     return (
         <Routes>
+
+            {/* Public pages */}
             <Route path="/" element={<Login />} />
             <Route path='/signup' element={<NewUser />} />
             <Route path='/resetpassword' element={<ResetPassword />} />
+
+            {/* Authenticated pages */}
+            <Route 
+                path='/dashboard' 
+                element={
+                    <Layout>
+                        <Dashboard />
+                    </Layout> 
+                }   
+            />
         </Routes>
     );
 }
