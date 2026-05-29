c7c3feee2da7c1cd659916ead3c4c06e44e9a53e
KingNeoIV <michael.rios.nw@hotmail.com>
2026-05-28 20:56:19 -0500

    refactor(app): update App layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import AppRouter from './router/AppRouter';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 export default function App() {
     return (
         <div className='min-h-screen flex flex-col'>
+            <Navbar />
+
             <main className="flex-1">
                 <AppRouter />
             </main>
