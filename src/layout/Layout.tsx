9446533f26c9f37a1640fd1a543a3566f7c50e41
KingNeoIV <michael.rios.nw@hotmail.com>
2026-05-28 20:56:32 -0500

    refactor(layout): simplify Layout wrapper

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,6 @@
-import Navbar from "../components/Navbar";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen flex flex-col">
-
-            {/* Top Navigation */}
-            <Navbar />
-
             {/* Page Content */}
             <main className="flex-1">
                 {children}
