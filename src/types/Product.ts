93116d94c3f43e1f667451783827a402f835e594
KingNeoIV <michael.rios.nw@hotmail.com>
2026-06-07 22:20:11 -0500

    types: add Product interface for API typing

---
+++ src/types/Product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: number;
+  title: string;
+  slug: string;
+  price: number;
+  description: string;
+  images: string[];
+  category: {
+    id: number;
+    name: string;
+    image: string;
+    slug: string;
+  };
+}
